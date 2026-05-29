684973689d3fd1e3fd9c817f1ab7fd3e136bc178
Less-Riv <d23390171@huauchinago.tecnm.mx>
2026-05-29 09:17:50 -0600

    Primer commit

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,9 +3,9 @@ import productsControllers from "../controllers/products.controller.js";
 
 const router = Router();
 
-router.get("/", productsControllers.getAll);
+router.get("/getAll", productsControllers.getAll);
 router.get("/:barcode", productsControllers.getOne);
-router.post("/", productsControllers.insertOne);
+router.post("/insertOne", productsControllers.insertOne);
 router.put("/updateOne/:barcode", productsControllers.updateOne);
 router.delete("/deleteOne/:barcode", productsControllers.deleteOne);
 
